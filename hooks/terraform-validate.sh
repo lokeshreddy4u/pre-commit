1dc48a0607190022f8011d927260c11c07635ec2
LOKESHREDDY KB <lokeshreddy4u@gmail.com>
2020-06-23 09:32:16 -0400

    Update terraform-validate.sh

--- hooks/terraform-validate.sh
+++ hooks/terraform-validate.sh
@@ -8,7 +8,7 @@ set -e
 export PATH=$PATH:/usr/local/bin
 
 for dir in $(echo "$@" | xargs -n1 dirname | sort -u | uniq); do
-  echo "$dir"
+  echo "directory: $dir"
   pushd "$dir" >/dev/null
   terraform init -backend=false
   terraform validate
